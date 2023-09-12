467e301695f3ad03512234eb5881744e03f9b3e6
Eduardo <ext.eduardo.braga@farfetch.com>
2023-09-12 21:56:51 +0100

    fix: adjusted deploy configs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/threejs-pontedluis',
+    base: '/ThreeJs-PonteDLuis',
     root: './',
     build: {
-        outDir: './threejs-pontedluis',
+        outDir: './dist',
         assetsDir: '',
     },
 });
